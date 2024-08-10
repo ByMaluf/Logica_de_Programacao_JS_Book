cca95f6aca09aa9b13ab0d5435f9331500a1e9e3
Brenno Ysrael M. L. Correia <brenno.maluf@gmail.com>
2024-08-10 19:54:43 -0400

    Programa Número perfeito.

---
+++ CAP-5/Numero Perfeito/main.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form');
+const resultado = document.querySelector('.resposta');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const numero = +form.num.value;
+  let total = 0;
+
+  resultado.innerHTML += `Números divisíveis: `
+
+  for (let i = 1; numero > i; i++) {
+    if (numero % i === 0) {
+      total += i
+      resultado.innerHTML += `${i} - `
+    }
+  }
+
+  if (total === numero) {
+    resultado.innerHTML += `\nTotal = ${total}\n${numero} é um número perfeito.`
+  } else {
+    resultado.innerHTML += `\nTotal = ${total}\n${numero} NÃO é um número perfeito.`
+  }
+})
+
+// const numero = form.num.value;
